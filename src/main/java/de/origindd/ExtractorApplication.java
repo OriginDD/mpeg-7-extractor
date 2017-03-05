73bf92d1651be227e234550726e0b5c399071324
OriginDD <ulrich@kowohl-designz.de>
2017-03-05 21:07:19 +0100

    Update ExtractorApplication.java

--- src/main/java/de/origindd/ExtractorApplication.java
+++ src/main/java/de/origindd/ExtractorApplication.java
@@ -13,13 +13,5 @@ public class ExtractorApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(ExtractorApplication.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 }
